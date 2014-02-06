1d16491e0e760957712a5bf2b2221e746970ef06
practicing01 <practicing01@gmail.com>
2014-02-06 02:08:23 -0400

    zxcvzxv

--- engine/source/Banana_Modifications/Struct_Module/Struct_Module_Functions/Function_Struct_Module_Main_Destroy.cc
+++ engine/source/Banana_Modifications/Struct_Module/Struct_Module_Functions/Function_Struct_Module_Main_Destroy.cc
@@ -1,8 +1,29 @@
 void Function_Struct_Module_Main_Destroy(struct Struct_Module *Pointer_Struct_Module)
 {
 
-//Free data list.
+if (*(Pointer_Struct_Module->Pointer_Linked_List_Struct_Data)!=NULL)
+{
 
+unsigned int *Pointer_Struct_Module_Linked_List_Node=
+(unsigned int*)(*(Pointer_Struct_Module->Pointer_Linked_List_Struct_Data));
+
+unsigned int *Pointer_Struct_Module_Linked_List_Node_Next;
+
+do
+{
+
+Pointer_Struct_Module_Linked_List_Node_Next=(unsigned int*)(*(Pointer_Struct_Module_Linked_List_Node+1));
+
+free(Pointer_Struct_Module_Linked_List_Node);
+
+Pointer_Struct_Module_Linked_List_Node=Pointer_Struct_Module_Linked_List_Node_Next;
+
+}
+while(Pointer_Struct_Module_Linked_List_Node!=(unsigned int*)(*(Pointer_Struct_Module_Linked_List_Node+1)));
+
+}
+
+/******************************************************************************/
 
 if (*(Pointer_Struct_Module->Pointer_Struct_Module_Linked_List_Parents)!=NULL)
 {
